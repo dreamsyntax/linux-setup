736547f788d70acfd580ce331ae987fbe1969b1f
dreamsyntax <dreamsyntax@gmail.com>
2023-12-11 15:08:32 -0700

    Update setup

--- endeavour/setup.sh
+++ endeavour/setup.sh
@@ -3,7 +3,8 @@
 ## For Cinnamon Edition of EndeavourOS
 
 ## Still need to figure out the themes for terminal
-## For now uncheck use system theme in Terminal -> Profile and use "white on black" with palette "solarized"
+# for xed, change to "Cobalt"
+# for terminal uncheck use system theme in Terminal -> Profile and use "white on black" with palette "solarized"
 
 ### Setup Configs ###
 # cp -R ./config/. ~/
@@ -14,17 +15,21 @@ yay -Syu --noconfirm
 ### Install software ###
 yay -S librewolf-bin --noconfirm
 sudo pacman -S nvidia-inst --noconfirm
+# laptop only
+yay -S envycontrol ## prompt so we can abort
+# end laptop only
 sudo pacman -S steam # this may have an important prompt 
 sudo pacman -S discord bluez bluez-utils --noconfirm
 sudo systemctl start bluetooth
 sudo systemctl enable bluetooth
 sudo pacman -S blueman kdenlive redshift --noconfirm
 sudo pacman -S breeze --noconfirm # for kde applications (kdenlive) not being dark theme by default
-yay -S protonup-qt-bin envycontrol appimagelauncher vscodium-bin mint-themes gnome-themes-extra --noconfirm
+yay -S protonup-qt-bin webcord-bin vscodium-bin mint-themes gnome-themes-extra --noconfirm
 sudo pacman -S blender godot --noconfirm
-
+# yay -S appimagelauncher ## Not really needed at this time, maybe later
 # systemctl mask --user redshift.service ## Probably unnecessary
 # yay -S unityhub ## Busted; logs out of Unity Hub each launch
+
 
 ### Install Flatpaks ###
 # need to install flatpak first, not installed by default
