c0349ca89d519bd36dbcbc6079b2d680c468ad8f
dreamsyntax <dreamsyntax@gmail.com>
2025-07-28 00:24:57 -0700

    Update cachyos setup

--- setup-cachyos-kde.sh
+++ setup-cachyos-kde.sh
@@ -5,6 +5,9 @@
 # Settings -> Touchpad -> Invert scroll direction (Natural scrolling)
 # Settings -> General Behavior -> Middle Mouse "Pastes selected text" -> Uncheck
 
+# Settings -> System Tray Settings -> Entries -> Bluetooth -> Always shown
+# Settings -> System Tray Settings -> Entries -> Power and Battery -> Always shown
+
 # SSD trim scheduler
 sudo systemctl enable --now fstrim.timer
 
@@ -14,6 +17,7 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 # for AppImage support
 sudo pacman -S --noconfirm fuse2
+# for portable AppImages just create `AppImageName.AppImage.home` folder in the same directory as it before running
 
 # for downgrade command (use if a specific package breaks) ex: sudo downgrade kwin -> select older version with arrow keys/enter
 sudo pacman -S --noconfirm downgrade
@@ -30,8 +34,19 @@ sudo pacman -S 7zip
 
 # additional gaming packages
 # gamescope # https://github.com/ValveSoftware/gamescope
+# gamescope example gamescope -f -W 2560 -H 1440 -r 170 -- gamemoderun %command%
 # goverlay # https://github.com/benjamimgois/goverlay
 # mangohud # resource overlay
+
+# speedrun / capture and input capture packages
+sudo pacman -S --noconfirm love obs-studio
+sudo pacman -S lua51-sec # required for m-overlay
+sudo setcap cap_sys_ptrace=eip /usr/bin/love # required for m-overlay
+
+# Sunshine / Moonlight for Game Streaming / RDP
+# sunshine # for hosting https://github.com/LizardByte/Sunshine
+# qpwgraph # to cancel out other audio such as Discord for connected users
+# moonlight # client
 
 ## Disable the Discord / Vesktop & other electron apps from lowering/adjusting your Microphone gain:
 # Create local config, copying from latest to ~/.config/pipewire/pipewire-pulse.conf with:
@@ -40,6 +55,10 @@ mkdir -p ~/.config/pipewire/ && cp /usr/share/pipewire/pipewire-pulse.conf ~/.co
 # Edit, adding:
 #    {
 #       matches = [ { application.process.binary = "Discord" } ]
+#       actions = { quirks = [ block-source-volume ] }
+#    }
+#    {
+#       matches = [ { application.process.binary = "Vesktop" } ] # For Vesktop and other electron clients
 #       actions = { quirks = [ block-source-volume ] }
 #    }
 #    {
@@ -65,7 +84,17 @@ alias -s open "dolphin"
 # to delete: functions -e <aliasname>; funcsave <aliasname>
 
 ########## LAPTOP ONLY ##########
+
+# program to force iGPU mode
+sudo pacman -S supergfxctl
+sudo systemctl enable supergfxd
+sudo systemctl start supergfxd
+
+# Usage example:
+# supergfxctl --mode Integrated
+# supergfxctl --mode Hybrid
+
 ## laptop TAS2781 audio bug - Creates a service that auto resets the amp and device state to prevent the bug on audio source change
-## Bug still relevant as of 2025-05-27
+## Bug still relevant as of 2025-07-15
 # UNCOMMENT AFTER VERIFYING MANUALLY, NEVER AUTO RUN THIS https://github.com/DanielWeiner/tas2781-fix-16IRX8H
 # curl -s https://raw.githubusercontent.com/DanielWeiner/tas2781-fix-16IRX8H/refs/heads/main/install.sh | bash -s --
